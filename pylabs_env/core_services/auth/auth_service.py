9c7b752e658d38db9ac4a95dda209365f94096f2
pramit s shah <Pylabsinc@gmail.com>
2025-05-27 17:38:22 +0000

    Initial structure and Auth microservice added

---
+++ pylabs_env/core_services/auth/auth_service.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/auth/health')
+def health():
+    return jsonify({'status': 'auth service running'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
